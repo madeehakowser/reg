c3e2e7308e8cec43edd8ae649a5fd72910baaec6
madeehakowser <madiyakowser014@gmail.com>
2024-12-13 11:29:55 +0530

    Add files via upload

---
+++ game.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,sum;
+printf("Enter the A value");
+scanf("%d",&a);
+printf("Enter the b value");
+scanf("%d"&b);
+sum=a+b;
+printf("The sum of two number:%d",a,b,sum);
+B
+B
+B
+B
+B
+B
+B
+B
+B
+}
